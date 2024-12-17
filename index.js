4f4ed0dd3e133eba3d7025d39fd9b6bec788552e
nika380 <nikanatroshvili7@gmail.com>
2024-12-17 18:10:02 +0400

    chore: changes in htmlToPdf function: - change html parameter placement and type definition, add generation based on url and html content - add launchoptions parameter for puppeteer launch func

--- index.js
+++ index.js
@@ -2,18 +2,26 @@ const puppeteer = require("puppeteer");
 
 /**
  * Generates a PDF from HTML content.
+ * @param {{content: string, url: string}} html - The HTML content to render.
  * @param {import("puppeteer").PDFOptions} options - The PDF options.
- * @param {string} html - The HTML content to render.
+ * @param {import('puppeteer').LaunchOptions} launchOptions - Launch options for Puppeteer.
  */
-const htmlToPdf = async (options, html) => {
+const htmlToPdf = async (html, options = {}, launchOptions = {}) => {
   // Launches Chromium Browser
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch(launchOptions);
 
   // Opens New Page
   const page = await browser.newPage();
 
-  // Loands provided html content on page
-  await page.setContent(html, { waitUntil: "networkidle0" });
+  if (html.url) {
+    // Navigates to provided url page
+    await page.goto(html.url, { waitUntil: "domcontentloaded" });
+  } else if (html.content) {
+    // Loands provided html content on page
+    await page.setContent(html.content, { waitUntil: "networkidle0" });
+  } else {
+    throw Error("HTML Content Or URL Not Provided");
+  }
 
   // generates pdf based on provided information
   const data = await page.pdf(options);
